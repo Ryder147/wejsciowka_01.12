6eeb263f40ce6a58d5f0b5d7eefc9123544448da
Ryder147 <83040548+Ryder147@users.noreply.github.com>
2021-12-02 12:21:55 +0100

    zamieniłem Array na Object
    miałem inną wersję javy i wszystko pisałem na czuja teraz już powinno działać

--- src/main/java/pl/sggw/SimpleWzimBoundedQueue.java
+++ src/main/java/pl/sggw/SimpleWzimBoundedQueue.java
@@ -7,40 +7,24 @@ import java.util.Queue;
 public class SimpleWzimBoundedQueue<E> implements WzimBoundedQueue<E> {
 
 
+
     E[] tab;
     int max;
     // .
     public SimpleWzimBoundedQueue(final int maxCapacity) {
 
-    tab= (E[]) new Array[maxCapacity];
-    max=maxCapacity;
+        tab= (E[]) new Object[maxCapacity];
+        max=maxCapacity;
 
     }
-
-    @Override
+    
+@Override
     public boolean add(E a){
 
         for (int i = 0; i < tab.length; i++) {
             if(tab[i]==null){
                 tab[i]=a;
-            }
-            else{
-                if(i==tab.length-1 && tab[i]!=null){
-                   throw new IllegalStateException();
-                }
-            }
-        }
-
-
-        return true;
-
-    }
-    @Override
-    public boolean offer(E a){
-
-        for (int i = 0; i < tab.length; i++) {
-            if(tab[i]==null){
-                tab[i]=a;
+                return true;
             }
             else{
                 if(i==tab.length-1 && tab[i]!=null){
@@ -50,19 +34,38 @@ public class SimpleWzimBoundedQueue<E> implements WzimBoundedQueue<E> {
         }
 
 
-        return true;
+        return false;
 
     }
-    @Override
+@Override
+    public boolean offer(E a){
+
+        for (int i = 0; i < tab.length; i++) {
+            if(tab[i]==null){
+                tab[i]=a;
+                return true;
+
+            }
+
+
+        }
+
+
+        return false;
+
+    }
+@Override
     public E remove(){
         E a= tab[0];
 
-        E[] tab1= (E[]) new Array[tab.length];
+        E[] tab1= (E[]) new Object[tab.length];
         for (int i = 0; i < tab.length-1; i++) {
 
             tab1[i]=tab[i+1];
         }
         tab=tab1;
+
+
         return a;
 
 
@@ -70,41 +73,45 @@ public class SimpleWzimBoundedQueue<E> implements WzimBoundedQueue<E> {
 
 
     }
+    @Override
     public E poll(){
         E a= tab[0];
 
-        E[] tab1= (E[]) new Array[tab.length];
+        E[] tab1= (E[]) new Object[tab.length];
         for (int i = 0; i < tab.length-1; i++) {
 
             tab1[i]=tab[i+1];
         }
         tab=tab1;
+
         if (a!=null)
-        return a;
+            return a;
         else{
             return null;
         }
     }
+    @Override
     public E element(){
         return tab[0];
     }
+    @Override
     public E peek(){
         return tab[0];
     }
-
+    @Override
     public int size(){
         int licznik=0;
         for (int i = 0; i < tab.length; i++) {
             if(tab[i]!=null){
-               licznik+=1;
+                licznik+=1;
             }
         }
         return licznik;
     }
+    @Override
     public int capacity(){
         return max;
     }
-
 
 
 
